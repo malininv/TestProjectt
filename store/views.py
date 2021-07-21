457f55cceb35d8e939e5cfb94bde92a183c0e391
Vladislav <vladmalinin93@gmail.com>
2021-07-21 16:59:49 +0700

    заработало, первый пустой элемент

--- store/views.py
+++ store/views.py
@@ -20,26 +20,18 @@ def products_by_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
 
     subcategories = category.children.all()
-
-    products = []
-
-    if subcategories:
+    products = [category.products.all()]
+    while subcategories:
+        temp = []
         for subcategory in subcategories:
-            if subcategory.children.all():
-                subsubcategories = subcategory.children.all()
-                for subsubcategory in subsubcategories:
-                    products.append(subsubcategory.products.all())
-            products.append(subcategory.products.all())
-    else:
-        products = category.products.all()
-
+            products.extend(subcategory.products.all())
+            temp.extend(subcategory.children.all())
+        subcategories = temp
+    subcategories = category.children.all()
     context = {
         'products': products,
         'category': category,
         'subcategories': subcategories
-        # 'category_list': category_list,
-        #'r_product': r_product,
-        #'category_parents_ids': category_parents_ids
     }
 
     template = 'store/by_category.html'
