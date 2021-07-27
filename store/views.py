220d379dd2ae129980831218dda6a8d16bdebefb
Vladislav <vladmalinin93@gmail.com>
2021-07-27 12:46:04 +0700

    поиск, дронул category_detail, сделал все в одну view

--- store/views.py
+++ store/views.py
@@ -6,32 +6,28 @@ from django.core.paginator import Paginator
 from django.http import Http404
 
 
-def index(request):
-    products = Product.objects.all()
+def index(request, hierarchy=None):
     categories = Category.objects.filter(parent=None)
-    page_number = request.GET.get('page', 1)
-    paginator = Paginator(products, 2)
-    page = paginator.get_page(page_number)
-    context = {
-        'products': page.object_list,
-        'categories': categories,
-        'page_obj': page
-    }
+    category = None
+    all_parent_categories = None
+    query_search = request.GET.get('search', '')
+    if hierarchy:
+        slug = hierarchy.split('/')[-1]
+        category = get_object_or_404(Category, slug=slug)
+        all_parent_categories = get_all_parents(category)
+        if hierarchy.split('/') == [c.slug for c in all_parent_categories]:
+            if query_search:
+                products = [p for p in get_products_by_category(category) if query_search in p.name]
+            else:
+                products = get_products_by_category(category)
 
-    template = 'store/index.html'
-
-    return render(request, template, context)
-
-
-def category_detail(request, hierarchy=None):
-    slug = hierarchy.split('/')[-1]
-    category = get_object_or_404(Category, slug=slug)
-    all_parent_categories = get_all_parents(category)
-    categories = Category.objects.filter(parent=None)
-    if hierarchy.split('/') == [c.slug for c in all_parent_categories]:
-        products = get_products_by_category(category)
+        else:
+            raise Http404('Invalid url')
     else:
-        raise Http404('Invalid url')
+        if query_search:
+            products = Product.objects.filter(name__icontains=query_search)
+        else:
+            products = Product.objects.all()
 
     page_number = request.GET.get('page', 1)
     paginator = Paginator(products, 2)
@@ -42,9 +38,10 @@ def category_detail(request, hierarchy=None):
         'category': category,
         'page_obj': page,
         'all_parent_categories': all_parent_categories,
-        'categories': categories
+        'categories': categories,
+        'query_search': query_search
     }
 
-    template = 'store/category_detail.html'
+    template = 'store/index.html'
 
     return render(request, template, context)
