c6c1d2db2e0b7badd3071355b3667805279bbcbf
Vladislav <vladmalinin93@gmail.com>
2021-07-29 18:21:32 +0700

    подумать на pushstate

--- store/static/store/main.js
+++ store/static/store/main.js
@@ -2,22 +2,44 @@ $(document).ready(() => {
     $('#input-main').submit((e) => {
         e.preventDefault()
         const req = $('#input-main').serialize()
-        ajax(req)
+        $.ajax({
+            url: $('#main-wrapper').attr("data-ajax"),
+            data: req,
+            success: (response) => {
+                let products = $(response).filter('.product-wrapper').html()
+                let paginator = $(response).find('#next_button').attr('href')
+                let paginator2 = $(response).find('#prev_button').attr('href')
+
+                $('.product-wrapper').html(products)
+                $('#next_button').attr("href", paginator)
+                $('#prev_button').attr("href", paginator2)
+                $('.breadcrumbs').empty()
+            }
+        })
     })
 
     $('#next_button').click((e) => {
         e.preventDefault()
         const req = $('#next_button').attr("href").slice(1)
-        ajax(req)
+
+       $.ajax({
+            url: '',
+            data: req,
+            success: (response) => {
+                let products = $(response).filter('.product-wrapper').html()
+                let paginator = $(response).find('#next_button').attr('href')
+                let paginator2 = $(response).find('#prev_button').attr('href')
+                $('.product-wrapper').html(products)
+                $('#next_button').attr("href", paginator)
+                $('#prev_button').attr("href", paginator2)
+
+            }
+        })
     })
 
     $('#prev_button').click((e) => {
         e.preventDefault()
         const req = $('#prev_button').attr("href").slice(1)
-        ajax(req)
-    })
-
-    function ajax(req) {
         $.ajax({
             url: $('#main-wrapper').attr("data-url"),
             data: req,
@@ -25,13 +47,27 @@ $(document).ready(() => {
                 let products = $(response).filter('.product-wrapper').html()
                 let paginator = $(response).find('#next_button').attr('href')
                 let paginator2 = $(response).find('#prev_button').attr('href')
-                console.log(response)
-                console.log(paginator)
                 $('.product-wrapper').html(products)
                 $('#next_button').attr("href", paginator)
                 $('#prev_button').attr("href", paginator2)
-                history.replaceState(null, null, `?${req}`);
+
             }
         })
-    }
+    })
+
+    // function ajax(req, url) {
+    //     $.ajax({
+    //         url: $('#main-wrapper').attr("data-url"),
+    //         data: req,
+    //         success: (response) => {
+    //             let products = $(response).filter('.product-wrapper').html()
+    //             let paginator = $(response).find('#next_button').attr('href')
+    //             let paginator2 = $(response).find('#prev_button').attr('href')
+    //             $('.product-wrapper').html(products)
+    //             $('#next_button').attr("href", paginator)
+    //             $('#prev_button').attr("href", paginator2)
+    //
+    //         }
+    //     })
+    // }
 })
