2552456f97d27ca44188adeb4fcd90109804128c
Vladislav <vladmalinin93@gmail.com>
2021-07-29 15:11:10 +0700

    сократил js

--- store/static/store/main.js
+++ store/static/store/main.js
@@ -2,42 +2,22 @@ $(document).ready(() => {
     $('#input-main').submit((e) => {
         e.preventDefault()
         const req = $('#input-main').serialize()
-        $.ajax({
-            url: $('#main-wrapper').attr("data-url"),
-            data: req,
-            success: (response) => {
-                let products = $(response).filter('.product-wrapper').html()
-                let paginator = $(response).find('#next_button').attr('href')
-                let paginator2 = $(response).find('#prev_button').attr('href')
-                $('.product-wrapper').html(products)
-                $('#next_button').attr("href", paginator)
-                $('#prev_button').attr("href", paginator2)
-                history.replaceState(null, null, `?${req}`);
-            }
-        })
+        ajax(req)
     })
 
     $('#next_button').click((e) => {
         e.preventDefault()
         const req = $('#next_button').attr("href").slice(1)
-        $.ajax({
-            url: $('#main-wrapper').attr("data-url"),
-            data: req,
-            success: (response) => {
-                let products = $(response).filter('.product-wrapper').html()
-                let paginator = $(response).find('#next_button').attr('href')
-                let paginator2 = $(response).find('#prev_button').attr('href')
-                $('.product-wrapper').html(products)
-                $('#next_button').attr("href", paginator)
-                $('#prev_button').attr("href", paginator2)
-                history.replaceState(null, null, `?${req}`);
-            }
-        })
+        ajax(req)
     })
 
     $('#prev_button').click((e) => {
         e.preventDefault()
         const req = $('#prev_button').attr("href").slice(1)
+        ajax(req)
+    })
+
+    function ajax(req) {
         $.ajax({
             url: $('#main-wrapper').attr("data-url"),
             data: req,
@@ -51,6 +31,5 @@ $(document).ready(() => {
                 history.replaceState(null, null, `?${req}`);
             }
         })
-    })
-
+    }
 })
